4c8af713ffd64673edb8027f06284c141dbe8123
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-08-05 05:21:33 +0500

    added list ui, updated nav bar

--- face_detection/views.py
+++ face_detection/views.py
@@ -2,17 +2,17 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import FaceDetection
 from pages.models import Image
+
+
 class FaceDetectionListView(LoginRequiredMixin, generic.ListView):
     template_name = 'face_detection/face_detection_list.html'
     context_object_name = "faces"
 
-    
-    def get_queryset(self):
-        queryset = FaceDetection.objects.all()
-        images = Image.objects.filter(user=self.request.user)
-        queryset = [];
-        for image in images:
-            queryset = FaceDetection.objects.filter(image_fr=image.id)
-                
 
+    def get_queryset(self):
+        # getting images owned by current user
+        images = Image.objects.filter(user=self.request.user)
+        # getting face rekognition data using related image
+        queryset = FaceDetection.objects.filter(image_fr__in=images)
         return queryset
+    
